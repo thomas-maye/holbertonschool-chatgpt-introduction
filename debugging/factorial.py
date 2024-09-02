a3a4d3ac034d3de3ecdd28043cef5eb76067d28b
Thomas Mayé <167352697+thomas-maye@users.noreply.github.com>
2024-09-02 10:20:56 +0200

    Debbugging my code

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,2 +1,12 @@
-kbjdsfvhep
-KeyboardInterrupt
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1  # Décrémente n pour éviter une boucle infinie
+    return result
+
+f = factorial(int(sys.argv[1]))
+print(f)
